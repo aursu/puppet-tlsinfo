5ea9414c4f3e27f222c31e0e58a34ea08b34d9db
Alexander Ursu <alexander.ursu@gmail.com>
2018-09-04 00:48:44 +0200

    try tofix password

--- lib/puppet/type/sslkey.rb
+++ lib/puppet/type/sslkey.rb
@@ -88,13 +88,12 @@ Puppet::Type.newtype(:sslkey) do
     defaultto :true
   end
 
-  newparam(:password) do
+  newproperty(:password) do
     desc "Encrypted private key password"
 
     validate do |value|
       raise ArgumentError, _("Passwords cannot be empty") if value.is_a?(String) and value.empty?
     end
-
   end
 
   # copied from https://github.com/puppetlabs/puppet/blob/master/lib/puppet/type/file/mode.rb
@@ -197,7 +196,6 @@ Puppet::Type.newtype(:sslkey) do
       elsif value == :absent || (value.is_a?(String) && checksum?(value))
         fail Puppet::Error, 'Private key must be provided via :content property' unless @actual_content
       else
-        Puppet.info _("Private key password '%{password}'") % {password: resource.parameter(:password).value}
         begin
           OpenSSL::PKey::RSA.new(value)
         rescue OpenSSL::PKey::RSAError => e
