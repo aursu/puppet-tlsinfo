ecad067cb14966c1ce08d411b1331ebd60865372
Alexander Ursu <alexander.ursu@gmail.com>
2018-10-25 20:10:21 +0200

    Look for certificate in catalog

---
+++ lib/puppet/functions/tlsinfo/certpath.rb
@@ -0,0 +1,12 @@
+Puppet::Functions.create_function(:'tlsinfo::certpath') do
+    dispatch :certpath do
+        scope_param
+        param 'String', :cert
+    end
+
+    def certpath(scope, cert)
+        found = catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:sslcertificate)) && [r[:subject_hash], r[:path], r.title].include?(cert) }
+        return found[:path] if found
+        nil
+    end
+  end
