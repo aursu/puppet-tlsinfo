10436c059aab208ef7c20b6d0c531eaa3ed19283
Alexander Ursu <alexander.ursu@gmail.com>
2018-10-29 23:41:43 +0100

    Added tlsinfo::certificate

---
+++ manifests/certificate.pp
@@ -0,0 +1,28 @@
+# A description of what this defined type does
+#
+# @summary A short summary of the purpose of this defined type.
+#
+# @example
+#   tlsinfo::certificate { 'namevar': }
+define tlsinfo::certificate (
+    Optional[String]
+            $cert     = undef,
+    Optional[Stdlib::Unixpath]
+            $basepath = $tlsinfo::basepath,
+) {
+    if $cert {
+        $certdata = $cert
+    }
+    else {
+        $certdata = lookup("${name}_certificate", Optional[String], 'first', undef)
+    }
+
+    unless $certdata {
+        fail("Certificate data does not exists. Please specify either parameter \$cert or Hiera key \"${name}_certificate\"")
+    }
+
+    $certpath = tlsinfo::certpath($certdata, $basepath)
+    sslcertificate { $certpath:
+      content => $certdata,
+    }
+}
