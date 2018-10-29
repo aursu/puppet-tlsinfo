91d113e4ff428f8536801cdb3d0cbcc57fef085f
Alexander Ursu <alexander.ursu@gmail.com>
2018-10-29 23:00:01 +0100

    Fixed wrong variable use

--- lib/puppet/functions/tlsinfo/certpath.rb
+++ lib/puppet/functions/tlsinfo/certpath.rb
@@ -17,13 +17,13 @@ Puppet::Functions.create_function(:'tlsinfo::certpath') do
     def basename(cert)
         certobj = read_x509_cert(cert)
 
-        basicconstraints, = cert.extensions.select { |e| e.oid == 'basicConstraints' }.map { |e| e.to_h }
-        cn, = cert.subject.to_a.select { |name, _data, _type| name == 'CN' }
+        basicconstraints, = certobj.extensions.select { |e| e.oid == 'basicConstraints' }.map { |e| e.to_h }
+        cn, = certobj.subject.to_a.select { |name, _data, _type| name == 'CN' }
         _name, data, _type = cn
 
         if basicconstraints && basicconstraints['value'].include?('CA:TRUE')
             # basename is Certificate subject hash
-            cert.subject.hash.to_s(16)
+            certobj.subject.hash.to_s(16)
         else
             data.sub('*', 'wildcard')
         end
