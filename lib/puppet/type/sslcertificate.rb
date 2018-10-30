8bab84c07a59874271e968d41296f9d7e18e8b30
Alexander Ursu <aursu@mac.local>
2018-10-30 14:49:02 +0100

    Returned resource title as key for catalog lookup

--- lib/puppet/type/sslcertificate.rb
+++ lib/puppet/type/sslcertificate.rb
@@ -662,7 +662,7 @@ Puppet::Type.newtype(:sslcertificate) do
   def lookupcatalog(key)
     return nil unless catalog
     # path, subject_hash and title are all key values
-    catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:sslcertificate)) && [r[:subject_hash], r[:path]].include?(key) }
+    catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:sslcertificate)) && [r[:subject_hash], r[:path], r.title].include?(key) }
   end
 
   # return OpenSSL::X509::Certificate representation of content property
