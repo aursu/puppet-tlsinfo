641958d2869b0651dac4890e8a7f9d0923fe5964
Alexander Ursu <aursu@mac.local>
2018-10-30 22:33:57 +0100

    debug it

--- manifests/certpair.pp
+++ manifests/certpair.pp
@@ -88,6 +88,6 @@ define tlsinfo::certpair (
         content => $certdata,
         pkey    => $keypath,
         # cacert  => $cacert,
-        cacert  => 'LetsEncryptAuthorityX3',
+        cacert  => '/etc/pki/tls/certs/4a0a35c0.pem',
     }
 }
