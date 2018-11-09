b8830be40f57e196c0e70bf554e761bdb6764bc7
Alexander Ursu <aursu@mac.local>
2018-11-09 17:49:25 +0100

    Debug it

--- lib/puppet/functions/tlsinfo/getcertpath.rb
+++ lib/puppet/functions/tlsinfo/getcertpath.rb
@@ -6,8 +6,7 @@ Puppet::Functions.create_function(:'tlsinfo::getcertpath') do
     def lookupcatalog(key)
         catalog = closure_scope.catalog
         # path, subject_hash and title are all key values
-        catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:sslcertificate)) &&
-                                     [r[:subject_hash], r[:subject_hash_old], r[:path], r.title].include?(key) }
+        catalog.resources.find { |r| r.is_a?(Puppet::Type.type(:sslcertificate))  }
     end
 
     def certpath(certpairname)
