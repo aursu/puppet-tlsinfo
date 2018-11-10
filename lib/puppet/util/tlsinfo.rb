bdaf4cd8cb241ac6ec96bdd783bcb85a2b4104a4
Alexander Ursu <aursu@mac.local>
2018-11-11 00:24:23 +0100

    added basename and normalize functions

--- lib/puppet/util/tlsinfo.rb
+++ lib/puppet/util/tlsinfo.rb
@@ -10,4 +10,23 @@ module Puppet::Util::TlsInfo
         nil
     end
 
+    def basename(cert)
+        certobj = read_x509_cert(cert)
+
+        basicconstraints, = certobj.extensions.select { |e| e.oid == 'basicConstraints' }.map { |e| e.to_h }
+        cn, = certobj.subject.to_a.select { |name, _data, _type| name == 'CN' }
+        _name, data, _type = cn
+
+        if basicconstraints && basicconstraints['value'].include?('CA:TRUE')
+            # basename is Certificate subject hash
+            certobj.subject.hash.to_s(16)
+        else
+            data.sub('*', 'wildcard')
+        end
+    end
+
+    def normalize(name)
+        name.sub('*', 'wildcard').gsub('.', '_').gsub("'", '_').gsub(' ', '_')
+    end
+
 end
