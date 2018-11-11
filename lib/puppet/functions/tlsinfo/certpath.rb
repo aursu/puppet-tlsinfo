7571771c5dd68d984d36d486e1d092f6acd086ef
Alexander Ursu <aursu@mac.local>
2018-11-11 18:37:40 +0100

    One more try

--- lib/puppet/functions/tlsinfo/certpath.rb
+++ lib/puppet/functions/tlsinfo/certpath.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', '..'))
 require 'puppet/util/tlsinfo'
 
 Puppet::Functions.create_function(:'tlsinfo::certpath') do
-    include Puppet::Util::TlsInfo
+    extend Puppet::Util::TlsInfo
 
     dispatch :certpath do
         param 'String', :cert
--- lib/puppet/functions/tlsinfo/keypath.rb
+++ lib/puppet/functions/tlsinfo/keypath.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', '..'))
 require 'puppet/util/tlsinfo'
 
 Puppet::Functions.create_function(:'tlsinfo::keypath') do
-    include Puppet::Util::TlsInfo
+    extend Puppet::Util::TlsInfo
 
     dispatch :keypath do
         param 'String', :cert
