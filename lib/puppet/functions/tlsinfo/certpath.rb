bda8f1c77436d41dc234cd93770d2a8bd1e42add
Alexander Ursu <aursu@mac.local>
2018-11-11 22:57:29 +0100

    call ruby module functions

--- lib/puppet/functions/tlsinfo/certpath.rb
+++ lib/puppet/functions/tlsinfo/certpath.rb
@@ -3,7 +3,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', '..'))
 require 'puppet/util/tlsinfo'
 
 Puppet::Functions.create_function(:'tlsinfo::certpath') do
-    extend Puppet::Util::TlsInfo
 
     dispatch :certpath do
         param 'String', :cert
@@ -11,7 +10,7 @@ Puppet::Functions.create_function(:'tlsinfo::certpath') do
     end
 
     def certpath(cert, basepath = '/etc/pki/tls/certs')
-        base = basename(cert)
+        base = Puppet::Util::TlsInfo.basename(cert)
         "#{basepath}/#{base}.pem"
     end
   end
--- lib/puppet/functions/tlsinfo/keypath.rb
+++ lib/puppet/functions/tlsinfo/keypath.rb
@@ -3,7 +3,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', '..'))
 require 'puppet/util/tlsinfo'
 
 Puppet::Functions.create_function(:'tlsinfo::keypath') do
-    extend Puppet::Util::TlsInfo
 
     dispatch :keypath do
         param 'String', :cert
@@ -11,7 +10,7 @@ Puppet::Functions.create_function(:'tlsinfo::keypath') do
     end
 
     def keypath(cert, basepath = '/etc/pki/tls/private')
-        base = basename(cert)
+        base = Puppet::Util::TlsInfo.basename(cert)
         "#{basepath}/#{base}.key"
     end
   end
