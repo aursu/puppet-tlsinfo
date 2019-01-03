aaf540aa320aab1cbf00d15d7e56828df29b2fd1
Alexander Ursu <alexander.ursu@gmail.com>
2019-01-03 17:59:25 +0100

    Added unit test for default behaviour of tlsinfo::certpair and tlsinfo::certificate defined types

--- spec/defines/certificate_spec.rb
+++ spec/defines/certificate_spec.rb
@@ -9,8 +9,17 @@ describe 'tlsinfo::certificate' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) { "class {'tlsinfo': }" }
 
-      it { is_expected.to compile }
+      context 'with default parameters' do
+        let(:title) { 'www.domain.com' }
+
+        it {
+          is_expected
+            .to compile
+            .and_raise_error(%r{Certificate data does not exists. Please specify either parameter \$cert or Hiera key "www_domain_com_certificate"})
+        }
+      end
     end
   end
 end
--- spec/defines/certpair_spec.rb
+++ spec/defines/certpair_spec.rb
@@ -9,8 +9,17 @@ describe 'tlsinfo::certpair' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) { "class {'tlsinfo': }" }
 
-      it { is_expected.to compile }
+      context 'with default parameters' do
+        let(:title) { 'www.domain.com' }
+
+        it {
+          is_expected
+            .to compile
+            .and_raise_error(%r{Can not find www_domain_com_certificate in Hiera})
+        }
+      end
     end
   end
 end
