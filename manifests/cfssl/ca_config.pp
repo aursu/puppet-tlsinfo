2e7204ca6508ffba5284add0bb75dbfb58b7ad1b
Alexander Ursu <alexander.ursu@gmail.com>
2023-09-30 01:28:10 +0200

    Added CFSSL ca_config file setup defined type

---
+++ manifests/cfssl/ca_config.pp
@@ -0,0 +1,68 @@
+# @summary CA configuration file for CFSSL
+#
+# CA configuration file for CloudFlare's PKI/TLS toolkit 
+#
+# @param filename
+#   CA configuration file name (not a path)
+#
+# @param path
+#   Path to the directory where CA configuration file is located.
+#   Full path to CA configuration file if value ends with ".json"
+#   If value ends with "/" - it will be concidered as a directory explicitly
+#
+# @param default_expiry
+#   CA signing configuration default expire time (default is 43824h)
+#
+# @param config
+#   CA configuration to be written into file
+#
+# @example
+#   tlsinfo::cfssl::ca_config { 'namevar': }
+define tlsinfo::cfssl::ca_config (
+  String $filename = "${name}.json",
+  Optional[String] $path = undef,
+  Tlsinfo::TimeDuration $default_expiry = '43824h',
+  Tlsinfo::SigningProfile $default_profile = {
+    expiry => $default_expiry,
+  },
+  Hash[String, Tlsinfo::SigningProfile] $signing_profiles = {},
+) {
+  include bsys::params
+
+  # setup default expiry into default profile
+  if $default_profile['expiry'] {
+    $defined_default_profile = $default_profile
+  }
+  else {
+    $defined_default_profile = $default_profile + {
+      expiry => $default_expiry,
+    }
+  }
+
+  $config = {
+    signing => {
+      'default' => $defined_default_profile,
+      profiles  => $signing_profiles,
+    },
+  }
+
+  case $path {
+    /\.json$/: {
+      $config_path = $path
+    }
+    /\/$/: {
+      $config_path = "${path}${filename}"
+    }
+    String: {
+      $config_path = "${path}/${filename}"
+    }
+    default: {
+      $config_path = "${bsys::params::pkibase}/${filename}"
+    }
+  }
+
+  file { $config_path:
+    ensure  => file,
+    content => stdlib::to_json_pretty($config, true, {indent => '    ', space => ' '}),
+  }
+}
---
+++ spec/defines/cfssl/ca_config_spec.rb
@@ -0,0 +1,197 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'tlsinfo::cfssl::ca_config' do
+  let(:title) { 'namevar' }
+  let(:params) do
+    {
+    }
+  end
+
+  let(:default_content) do
+    <<-JSONDATA
+{
+    "signing": {
+        "default": {
+            "expiry": "43824h"
+        },
+        "profiles": {
+        }
+    }
+}
+JSONDATA
+  end
+
+  let(:sample_content) do
+    <<-JSONDATA
+{
+    "signing": {
+        "default": {
+            "expiry": "43824h"
+        },
+        "profiles": {
+            "kubernetes": {
+                "usages": [
+                    "signing",
+                    "key encipherment",
+                    "server auth",
+                    "client auth"
+                ],
+                "expiry": "43824h"
+            }
+        }
+    }
+}
+JSONDATA
+  end
+
+  let(:sample_default_profile) do
+    <<-JSONDATA
+{
+    "signing": {
+        "default": {
+            "usages": [
+                "signing",
+                "key encipherment",
+                "server auth",
+                "client auth"
+            ],
+            "expiry": "8760h"
+        },
+        "profiles": {
+        }
+    }
+}
+JSONDATA
+  end
+
+  let(:sample_default_profile_no_expire) do
+    <<-JSONDATA
+{
+    "signing": {
+        "default": {
+            "usages": [
+                "signing",
+                "key encipherment",
+                "server auth",
+                "client auth"
+            ],
+            "expiry": "43824h"
+        },
+        "profiles": {
+        }
+    }
+}
+JSONDATA
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      if os_facts[:os]['family'] == 'Debian'
+        it {
+          is_expected.to contain_file('/etc/ssl/namevar.json')
+            .with_content(default_content)
+        }
+      else
+        it {
+          is_expected.to contain_file('/etc/pki/tls/namevar.json')
+            .with_content(default_content)
+        }
+      end
+
+      context 'when path specified' do
+        let(:title) { 'ca_config' }
+        let(:params) do
+          {
+            path: '/etc/kubernetes/pki',
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/etc/kubernetes/pki/ca_config.json')
+        }
+      end
+
+      context 'when path ends with .json' do
+        let(:params) do
+          {
+            path: '/etc/kubernetes/pki/ca_config.json',
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/etc/kubernetes/pki/ca_config.json')
+        }
+      end
+
+      context 'when path ends with /' do
+        let(:params) do
+          {
+            path: '/etc/kubernetes/pki/',
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/etc/kubernetes/pki/namevar.json')
+        }
+      end
+
+      context 'when signing profiles specified' do
+        let(:params) do
+          {
+            signing_profiles: {
+              'kubernetes' => {
+                usages: ['signing', 'key encipherment', 'server auth', 'client auth'],
+                expiry: '43824h',
+              },
+            },
+            path: '/etc/kubernetes/pki/',
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/etc/kubernetes/pki/namevar.json')
+            .with_content(sample_content)
+        }
+      end
+
+      context 'when default profile specified' do
+        let(:params) do
+          {
+            default_profile: {
+              usages: ['signing', 'key encipherment', 'server auth', 'client auth'],
+              expiry: '8760h',
+            },
+            path: '/etc/kubernetes/pki/',
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/etc/kubernetes/pki/namevar.json')
+            .with_content(sample_default_profile)
+        }
+      end
+
+      context 'when default profile specified w/o expiry' do
+        let(:params) do
+          {
+            default_profile: {
+              usages: ['signing', 'key encipherment', 'server auth', 'client auth'],
+            },
+            path: '/etc/kubernetes/pki/',
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/etc/kubernetes/pki/namevar.json')
+            .with_content(sample_default_profile_no_expire)
+        }
+      end
+    end
+  end
+end
